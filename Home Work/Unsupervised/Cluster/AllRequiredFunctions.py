0fc7ac89fbeb9f2e32b7fbee520df74de583fef5
Amol <amolbkajale96@gmail.com>
2021-05-17 15:30:21 +0530

    Mpdified

--- Home Work/Unsupervised/Cluster/AllRequiredFunctions.py
+++
@@ -1,8 +0,0 @@
-def Encoder(df):
-    from sklearn.preprocessing import LabelEncoder
-    le = LabelEncoder()
-    for column in (df.columns):
-        if df[column].dtype == 'object':
-            df[column] = le.fit_transform(df[column])
-
-        
